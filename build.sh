e3d45db7cf1cb1c02d6a1f00a1121ce73106c553
Samuel Pascua <sgpascua@ngcp.ph>
2021-04-17 10:29:01 +0800

    Makefile: Force ARCH to be arm644

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ARCH=arm64
+CLANG_DIR=~/clang/bin/clang
 
 if [ ! -f out/.config ]; then
 	make O=out rmx2170_defconfig
@@ -8,6 +8,6 @@ else
 	make O=out oldconfig
 fi
 
-make O=out CC=~/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- \
+make O=out CC=$CLANG_DIR CLANG_TRIPLE=aarch64-linux-gnu- \
      CROSS_COMPILE=~/gcc-7.4.1/bin/aarch64-linux-gnu- \
      TARGET_PRODUCT=atoll -j4 Image.gz
